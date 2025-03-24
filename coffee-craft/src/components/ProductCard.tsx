d17bbbfe3f78749ea8f7269b337ac4f5ee09d797
minh le <117999133+sixxson@users.noreply.github.com>
2025-03-24 16:17:12 +0700

    Update component ProductCard

--- coffee-craft/src/components/ProductCard.tsx
+++ coffee-craft/src/components/ProductCard.tsx
@@ -20,10 +20,16 @@ const ProductCard: React.FC<ProductCardProps> = ({
     <Link
       href={`/product/${product.id}`}
       className={cn(
-        "group/item bg-white flex flex-col gap-3 relative",
+        "group/item bg-white flex flex-col gap-3 relative overflow-hidden",
         className
       )}
     >
+
+      <div className="absolute top-1 -left-5 z-20 bg-red-500 text-white text-xs font-semibold px-5 py-1 -rotate-45 ">
+        {product.active === true ? "NEW" : ""}
+      </div>
+
+
       {/* Số lượng đã bán */}
       <div className="group-hover/item:hidden absolute left-1 bottom-1/3 z-20 bg-white text-[#040707] text-xs font-bold px-2 py-1 rounded-lg shadow-md border-b-2 border-[#a52f21] box-shadow-custom ">
         Đã bán {Math.floor(Math.random() * 10000) + 100}+{" "}
@@ -42,23 +48,26 @@ const ProductCard: React.FC<ProductCardProps> = ({
             className="text-yellow-400 drop-shadow-sm"
           />
         </div>
-        <div className="col-span-1 relative ">
-          {/* Badge + Label */}
-          <div className=" grid grid-cols-2 grid-rows-2 items-start">
-            {/* Badge "100% Arabica Cầu Đất" */}
-            <p className="col-start-2 row-start-1  flex flex-col z-20 bg-black text-white py-2 text-center text-[0.6rem] rounded-lg leading-tight">
-              <span>100%</span>
-              Arabica
-              <br />
-              Cầu Đất
-            </p>
-            <img
-              className="col-start-2 row-start-2"
-              alt="Premium"
-              src="https://taynguyensoul.vn/wp-content/uploads/2022/04/premium-label-300.png"
-            />
+        {product.categoryId && product.categoryId === "4f8f6162-564a-4565-a743-bac08f1e0451" && (
+          <div className="col-span-1 relative ">
+            {/* Badge + Label */}
+            <div className=" grid grid-cols-2 grid-rows-2 items-start">
+              {/* Badge "100% Arabica Cầu Đất" */}
+              <p className="col-start-2 row-start-1 flex flex-col z-20 bg-black text-white py-2 text-center text-[0.6rem] rounded-lg leading-tight">
+                <span>100%</span>
+                Arabica
+                <br />
+                Cầu Đất
+              </p>
+              <img
+                className="col-start-2 row-start-2"
+                alt="Premium"
+                src="https://taynguyensoul.vn/wp-content/uploads/2022/04/premium-label-300.png"
+              />
+            </div>
           </div>
-        </div>
+        )}
+
       </div>
 
       {/* Hình ảnh sản phẩm */}
