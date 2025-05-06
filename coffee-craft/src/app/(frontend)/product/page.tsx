71b1ec7c6e267b0c6f79c072e7e562c90e185a2c
nlpn1408 <nlpn1481999@gmail.com>
2025-05-06 10:22:55 +0700

    Refactor AllProducts component to use Suspense for loading state

--- coffee-craft/src/app/(frontend)/product/page.tsx
+++ coffee-craft/src/app/(frontend)/product/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import NewsLetter from "../../../components/Home/NewsLetter";
 import Banner from "../../../components/AllProduct/Banner";
 import ShowPro from "../../../components/AllProduct/Show-pro";
@@ -7,14 +7,14 @@ import ProductFilter from "../../../components/AllProduct/ProductFilter";
 import { CardContent } from "../../../components/ui/card";
 const AllProducts: React.FC = () => {
   return (
-    <>
+    <Suspense fallback={<CardContent className="h-screen">Loading...</CardContent>}>
       <Banner />
       <section className="container min-h-screen lg:px-16 md:px-8 px-4 py-10">
         <ProductFilter />
         <ShowPro />
         <NewsLetter />
       </section>
-    </>
+    </Suspense>
   );
 };
 
